b2168f97aab8d8fb57f158362ea3aea447bbec90
Zheng Zhang <zhangzz1218@gmail.com>
2024-02-27 14:31:45 -0800

    map_pie

--- App/Pages/page2.py
+++ App/Pages/page2.py
@@ -7,12 +7,15 @@ import dash_bootstrap_components as dbc
 import plotly.graph_objs as go 
 from plotly.subplots import make_subplots
 from statsmodels.tsa.statespace.sarimax import SARIMAX
+from urllib.request import urlopen
+import json
 
 df = pd.read_csv('online_retail.csv')
+rfm = pd.read_csv('processed_rfm_model.csv')
 df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
 df['Total_Sale']=df['Quantity']*df['UnitPrice']
 df=df[df['Total_Sale']!=0]
-
+df=df.merge(rfm[['CustomerID', 'Cluster']], on='CustomerID', how='left')
 
 # # Customize the layout
 # fig_trend.update_layout(
@@ -55,7 +58,8 @@ trend_fig.add_trace(
     go.Bar(         
         x=monthly_sales.index[-3:],          
         y=monthly_sales[-3:],        
-        marker=dict(color='rgba(255, 0, 0, 0.5)')
+        marker=dict(color='rgba(255, 0, 0, 0.5)'),
+        name='Prediction'
         ))
 trend_fig.add_trace(     
     go.Scatter(x=monthly_sales.index, y=monthly_sales, name='Sales Trend'),     
@@ -64,9 +68,30 @@ trend_fig.add_trace(
 trend_fig.update_layout(
     title='Sales Trend',
     xaxis_title='Month',
-    yaxis_title='Sales',
+    yaxis_title='Sales'
 )
+# pie
+cluster_counts = df['Cluster'].value_counts().reset_index()
+cluster_counts.columns = ['Cluster', 'count']
+pie_fig = px.pie(cluster_counts, 
+                            values='count', 
+                            names='Cluster', 
+                            title='Cluster Segmentation')
 
+# map 
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+
+map_fig = px.choropleth_mapbox(
+    data_frame = df,   
+    geojson=counties, 
+    locations='Country', color='Cluster',
+    color_continuous_scale="Viridis",
+    mapbox_style="carto-positron",
+    zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
+    opacity=0.5,
+    labels={'Total_Sale':'Sales'}
+    )
 
 dash.register_page(__name__, path='/page-1') # '/' is home page
 
@@ -87,12 +112,18 @@ layout = html.Div(
         #         )
         #     ]
         # ),
-        dcc.Graph(id='sales_trend_ts', figure=trend_fig)
+        dcc.Graph(id='sales_trend_ts', figure=trend_fig),
+
+        dbc.Row([
+            dbc.Col([dcc.Graph(id='sales_map', figure=map_fig)]),
+            dbc.Col([dcc.Graph(id='cluster_pie',figure=pie_fig)])            
+        ])
     ]
 )
 
 @callback(
-    [Output('sales_trend_ts', 'figure')],
+    [Output('sales_trend_ts', 'figure'),
+     Output('cluster_pie', 'figure')],
     [Input('country-selector', 'value')]
 )
 def update_sales_trend(selected_country):
@@ -121,7 +152,8 @@ def update_sales_trend(selected_country):
         go.Bar(                  
             x=monthly_sales.index[-3:],                   
             y=monthly_sales[-3:],                 
-            marker=dict(color='rgba(255, 0, 0, 0.5)')     
+            marker=dict(color='rgba(255, 0, 0, 0.5)'),
+            name='Prediction'     
             )) 
     trend_fig.add_trace(          
         go.Scatter(x=monthly_sales.index, y=monthly_sales, name='Sales Trend'),          
@@ -130,6 +162,32 @@ def update_sales_trend(selected_country):
     trend_fig.update_layout(     
         title='Sales Trend',     
         xaxis_title='Month',     
-        yaxis_title='Sales', 
+        yaxis_title='Sales'
         )
-    return [trend_fig]
+    
+    cluster_counts = filtered_df['Cluster'].value_counts().reset_index()
+    cluster_counts.columns = ['Cluster', 'count']
+    pie_fig=px.pie(cluster_counts, 
+                            values='count', 
+                            names='Cluster', 
+                            title='Cluster Segmentation'
+                              )
+    return [trend_fig, pie_fig]
+
+# @callback(
+#    Output('sales_map', 'figure'),
+#    [Input('cluster-slider', 'value')]
+#)
+#def update_sales_map(selected_cluster):
+#    filtered_df = df[df['clusters'] == selected_cluster]
+#    
+#    map_fig = px.scatter_mapbox(filtered_df,
+#                            lat="Lat",
+#                            lon="Lon",
+#                            size="Sales",
+#                            color="clusters",
+#                            size_max=15,
+#                            zoom=10,
+#                            mapbox_style="open-street-map")
+    
+#    return map_fig
