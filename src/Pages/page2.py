dc772de0765a6d18e6966bc022fe88a023f8a1a2
Zheng Zhang <zhangzz1218@gmail.com>
2024-03-07 12:19:59 -0800

    Deploy to Heroku for app update - dashtools

--- src/Pages/page2.py
+++ src/Pages/page2.py
@@ -8,15 +8,11 @@ import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 import pycountry
-import pathlib
-import pmdarima as pm
+import os
 
-
-PATH = pathlib.Path(__file__).parent.parent.parent
-DATA_PATH = PATH.joinpath('data').resolve()
-df = pd.read_csv(DATA_PATH.joinpath('raw/online_retail.csv'))
-rfm = pd.read_csv(DATA_PATH.joinpath('processed/processed_rfm_model.csv'))
-
+path = os.path.dirname(os.path.abspath(__file__))
+df = pd.read_csv(path + '/../../data/raw/online_retail.csv')
+rfm = pd.read_csv(path + '/../../data/processed/processed_rfm_model.csv')
 df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
 df['Total_Sale']=df['Quantity']*df['UnitPrice']
 df=df.merge(rfm[['CustomerID', 'Cluster']], on='CustomerID', how='left')
@@ -27,21 +23,6 @@ df['Year'] = df['InvoiceDate'].dt.year
 years = df['Year'].unique()
 years.sort()
 
-
-df1 = df
-df_ts = df1[['InvoiceDate', 'Total_Sale']]
-df_ts.set_index('InvoiceDate', inplace=True)
-day_sales = df_ts['Total_Sale'].resample('D').sum()
-day_diff_sales = day_sales.diff().dropna()
-
-model = SARIMAX(day_diff_sales, order=(0,0,2), seasonal_order=(1,0,2,7)) 
-model_fit = model.fit(disp=False)  
-forecast = model_fit.forecast(steps=82)
-forecast_df = pd.DataFrame(forecast)
-forecast_df.rename(columns={'predicted_mean': 'Total_Sale'}, inplace=True)
-df_ts = df_ts.combine_first(forecast_df)
-
-monthly_sales = df_ts['Total_Sale'].resample('M').sum()
 
 def get_iso_alpha_3(country_name):
     try:
@@ -62,56 +43,17 @@ external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 dash.register_page(__name__, path='/page-1', external_stylesheets=external_stylesheets) # '/' is home page
 
-cluster_mapping = {
-    i: cluster for i, cluster in enumerate(
-        ['lost customer', 'loyal customer', 'new customer', 'potential loyal customer'])}
+cluster_mapping = {0: 'total customer', 1: 'lost customer', 2: 'loyal customer', 
+                   3: 'new customer', 4: 'potential loyal customer'}
+    #{i: cluster for i, cluster in enumerate(
+    #    ['lost customer', 'loyal customer', 'new customer', 'potential loyal customer'])}
 
 country_sales = df.groupby('iso_alpha').agg({'Total_Sale':'sum'}).reset_index()
 country_sales['country_name'] = country_sales['iso_alpha'].apply(iso_to_country_name)
 country_name_mapping = df[['iso_alpha', 'Country']].drop_duplicates().set_index('iso_alpha')['Country'].to_dict()
 
-# # Create a choropleth map
-# global_map_fig = px.choropleth(country_sales,
-#                                locations='iso_alpha',
-#                                color='Total_Sale',
-#                                hover_name='country_name',
-#                                projection='natural earth',
-#                                title='Map Selector')
-
-# global_map_fig.update_layout(
-#     width=800,  # or whatever width suits your layout
-#     height=600,  # or whatever height suits your layout
-# )
-
-# # Adjust the position of the color bar
-# global_map_fig.update_layout(
-#     coloraxis_colorbar=dict(
-#         x=-0.15,  # Values are between -2 and 3, adjust as needed to move the color bar closer
-#         y=0.5,
-#         lenmode='fraction',  # Use 'fraction' to scale the color bar size relative to the plot
-#         len=0.7,  # Length of the color bar (70% of the plot height in this case)
-#     )
-# )
 
 layout = html.Div([
-    # dcc.Dropdown(
-    #     id='country-selector',
-    #     options=[{'label': country, 'value': country} for country in df['Country'].unique()],
-    #     value=df['Country'].unique()[0]  
-    # ),
-    # dcc.Graph(id='global-map'),
-    # dbc.Row([
-    #     dbc.Col([
-    #         dcc.Slider(
-    #             id='cluster-slider',
-    #             min=0,
-    #             max=len(cluster_mapping) - 1,  # Since index starts at 0
-    #             value=0,  # Default value
-    #             marks={i: name for i, name in cluster_mapping.items()},  # Assuming cluster_mapping is a list or similar
-    #             step=1  # Force selection of only integer positions
-    #         )
-    #     ], width=8),
-    # ]),
     dbc.Row(
         dbc.Col(
             dcc.Graph(id='global-map', style={'margin-bottom': '0px'}),  # Adjust bottom margin as needed
@@ -125,9 +67,10 @@ layout = html.Div([
                 dcc.Slider(
                     id='cluster-slider',
                     min=0,
-                    max=len(cluster_mapping) - 1,  # Since index starts at 0
+                    max=len(cluster_mapping) -1 ,  # Since index starts at 0
                     value=0,  # Default value
-                    marks={i: name for i, name in cluster_mapping.items()},  # Assuming cluster_mapping is a dict
+                    marks={i: name for i, name in cluster_mapping.items()},
+                    # Assuming cluster_mapping is a dict
                     step=1  # Force selection of only integer positions
                 ),
                 style={'margin-top': '0px', 'margin-bottom': '0px'}  # Adjust top and bottom margin as needed
@@ -170,6 +113,112 @@ def update_figures(clickData):
 
         df_ts = filtered_df[['InvoiceDate', 'Total_Sale']]
         df_ts['InvoiceDate'] = pd.to_datetime(df_ts['InvoiceDate'])
+
+        if df_ts['InvoiceDate'].dt.date.nunique() == 1:
+            df_ts.set_index('InvoiceDate', inplace=True)
+            monthly_sales = df_ts['Total_Sale'].resample('M').sum()
+            trend_fig = go.Figure()
+            trend_fig.add_trace(
+                go.Bar(x=monthly_sales.index, 
+                       y=monthly_sales.values, 
+                       marker=dict(color='rgba(248,191,93,0.7)'), 
+                       name='Monthly Sales'))
+            trend_fig.update_layout(
+                **base_layout,    
+                title={
+                'text': f'Sales Trend for {selected_country}',
+                'y':0.9,
+                'x':0.5,
+                'xanchor': 'center',
+                'yanchor': 'top',
+                'font': {
+                    #'family': 'Arial, sans-serif',
+                    'size':24,
+                
+                }
+            },     
+            xaxis_title='Month',     
+            yaxis_title='Sales',
+            )
+        else:
+            df_ts.set_index('InvoiceDate', inplace=True)
+            day_sales = df_ts['Total_Sale'].resample('D').sum()
+            day_diff_sales = day_sales.diff().dropna()
+            model = SARIMAX(day_diff_sales, order=(0,0,2), seasonal_order=(1,0,2,7)) 
+            model_fit = model.fit(disp=False)  
+            forecast = model_fit.forecast(steps=82)
+            forecast_df = pd.DataFrame(forecast)
+            forecast_df.rename(columns={'predicted_mean': 'Total_Sale'}, inplace=True)
+            df_ts = df_ts.combine_first(forecast_df)
+            monthly_sales = df_ts['Total_Sale'].resample('M').sum()
+
+            trend_fig = make_subplots(specs=[[{"secondary_y": False}]]) 
+            trend_fig.add_trace(          
+            go.Bar(x=monthly_sales.index[:-3],             
+                y=monthly_sales[:-3],
+                marker=dict(color='rgba(248,191,93,0.7)'),             
+                name='Monthly Sales'),          
+                secondary_y=False,      
+                ) 
+            trend_fig.add_trace(          
+            go.Bar(                  
+                x=monthly_sales.index[-3:],                   
+                y=monthly_sales[-3:],                 
+                marker=dict(color='rgba(255, 0, 0, 0.5)'),
+                name='Prediction'     
+                )) 
+            trend_fig.add_trace(          
+            go.Scatter(x=monthly_sales.index, y=monthly_sales, line=dict(color='rgba(0,143,140,1)')),          
+            secondary_y=False,           
+            ) 
+    
+            trend_fig.update_layout(
+                **base_layout,    
+                title={
+                'text': f'Sales Trend for {selected_country}',
+                'y':0.9,
+                'x':0.5,
+                'xanchor': 'center',
+                'yanchor': 'top',
+                'font': {
+                    #'family': 'Arial, sans-serif',
+                    'size':24,
+                
+                }
+            },     
+            xaxis_title='Month',     
+            yaxis_title='Sales',
+            )
+
+        cluster_counts = filtered_df['Cluster'].value_counts().reset_index()
+        cluster_counts.columns = ['Cluster', 'count']
+        custom_colors = ['#dc541b','#2e7d5e','#de9f37','#FFF7C5']
+        pie_fig=px.pie(cluster_counts, 
+                values='count', 
+                names='Cluster',
+                color_discrete_sequence=custom_colors                  
+                )
+        pie_fig.update_layout(
+            **base_layout,
+            margin=dict(l=0, r=0, t=20, b=20),
+            title={
+                'text': f'Costumer Segmentation <br> for {selected_country}',
+                'y':0.9,
+                'x':0.43,
+                'xanchor': 'center',
+                'yanchor': 'top',
+                'font': {
+                    #'family': 'Arial, sans-serif',
+                    'size':20,
+                }
+            },
+        )
+    
+        return trend_fig, pie_fig
+    else:
+        # Return default figures
+        df1 = df
+        df_ts = df1[['InvoiceDate', 'Total_Sale']]
         df_ts.set_index('InvoiceDate', inplace=True)
         day_sales = df_ts['Total_Sale'].resample('D').sum()
         day_diff_sales = day_sales.diff().dropna()
@@ -185,54 +234,54 @@ def update_figures(clickData):
         trend_fig = make_subplots(specs=[[{"secondary_y": False}]]) 
         trend_fig.add_trace(          
         go.Bar(x=monthly_sales.index[:-3],             
-               y=monthly_sales[:-3],
-               marker=dict(color='rgba(248,191,93,0.7)'),             
-               name='Monthly Sales'),          
-               secondary_y=False,      
-               ) 
+                y=monthly_sales[:-3],
+                marker=dict(color='rgba(248,191,93,0.7)'),             
+                name='Monthly Sales'),          
+                secondary_y=False,      
+                ) 
         trend_fig.add_trace(          
-        go.Bar(                  
-            x=monthly_sales.index[-3:],                   
-            y=monthly_sales[-3:],                 
-            marker=dict(color='rgba(255, 0, 0, 0.5)'),
-            name='Prediction'     
-            )) 
+            go.Bar(                  
+                x=monthly_sales.index[-3:],                   
+                y=monthly_sales[-3:],                 
+                marker=dict(color='rgba(255, 0, 0, 0.5)'),
+                name='Prediction'     
+                )) 
         trend_fig.add_trace(          
-        go.Scatter(x=monthly_sales.index, y=monthly_sales, line=dict(color='rgba(0,143,140,1)')),          
-        secondary_y=False,           
-        ) 
+            go.Scatter(x=monthly_sales.index, y=monthly_sales, line=dict(color='rgba(0,143,140,1)')),          
+            secondary_y=False,           
+            ) 
     
         trend_fig.update_layout(
-            **base_layout,    
-            title={
-            'text': f'Sales Trend for {selected_country}',
-            'y':0.9,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top',
-            'font': {
-                #'family': 'Arial, sans-serif',
-                'size':24,
+                **base_layout,    
+                title={
+                'text': f'Sales Trend in Total',
+                'y':0.9,
+                'x':0.5,
+                'xanchor': 'center',
+                'yanchor': 'top',
+                'font': {
+                    #'family': 'Arial, sans-serif',
+                    'size':24,
                 
-            }
-        },     
-        xaxis_title='Month',     
-        yaxis_title='Sales',
-        )
-
-        cluster_counts = filtered_df['Cluster'].value_counts().reset_index()
+                }
+            },     
+            xaxis_title='Month',     
+            yaxis_title='Sales',
+            )
+        
+        cluster_counts = df['Cluster'].value_counts().reset_index()
         cluster_counts.columns = ['Cluster', 'count']
         custom_colors = ['#dc541b','#2e7d5e','#de9f37','#FFF7C5']
         pie_fig=px.pie(cluster_counts, 
-                   values='count', 
-                   names='Cluster',
-                   color_discrete_sequence=custom_colors                  
-                   )
+                values='count', 
+                names='Cluster',
+                color_discrete_sequence=custom_colors                  
+                )
         pie_fig.update_layout(
             **base_layout,
             margin=dict(l=0, r=0, t=20, b=20),
             title={
-                'text': f'Costumer Segmentation <br> for {selected_country}',
+                'text': f'Costumer Segmentation <br> in Total',
                 'y':0.9,
                 'x':0.43,
                 'xanchor': 'center',
@@ -242,107 +291,23 @@ def update_figures(clickData):
                     'size':20,
                 }
             },
-    )
-    
+        )
+
         return trend_fig, pie_fig
-    else:
-        # Return default empty figures
-        fig_bar = go.Figure()
-        fig_bar.update_layout(**base_layout)
-    
-        fig_pie = go.Figure()
-        fig_pie.update_layout(**base_layout)
-        return fig_bar, fig_pie
         
-    
-# def update_sales_trend(selected_country):
-#     filtered_df = df[df['Country'] == selected_country]
-#     df_ts = filtered_df[['InvoiceDate', 'Total_Sale']]
-#     df_ts.set_index('InvoiceDate', inplace=True)
-#     day_sales = df_ts['Total_Sale'].resample('D').sum()
-#     day_diff_sales = day_sales.diff().dropna()
-
-#     model = SARIMAX(day_diff_sales, order=(0,0,2), seasonal_order=(1,0,2,7)) 
-#     model_fit = model.fit(disp=False)  
-#     forecast = model_fit.forecast(steps=82)
-#     forecast_df = pd.DataFrame(forecast)
-#     forecast_df.rename(columns={'predicted_mean': 'Total_Sale'}, inplace=True)
-#     df_ts = df_ts.combine_first(forecast_df)
-
-#     monthly_sales = df_ts['Total_Sale'].resample('ME').sum() 
-#     trend_fig = make_subplots(specs=[[{"secondary_y": False}]]) 
-#     trend_fig.add_trace(          
-#         go.Bar(x=monthly_sales.index[:-3],             
-#                y=monthly_sales[:-3],
-#                marker=dict(color='rgba(27, 171, 210, 0.7)'),             
-#                name='Monthly Sales'),          
-#                secondary_y=False,      
-#                ) 
-#     trend_fig.add_trace(          
-#         go.Bar(                  
-#             x=monthly_sales.index[-3:],                   
-#             y=monthly_sales[-3:],                 
-#             marker=dict(color='rgba(255, 0, 0, 0.5)'),
-#             name='Prediction'     
-#             )) 
-#     trend_fig.add_trace(          
-#         go.Scatter(x=monthly_sales.index, y=monthly_sales, line=dict(color='rgba(0,143,140,1)')),          
-#         secondary_y=False,           
-#         ) 
-    
-#     trend_fig.update_layout(    
-#         title={
-#             'text': f'Sales Trend for {selected_country}',
-#             'y':0.9,
-#             'x':0.5,
-#             'xanchor': 'center',
-#             'yanchor': 'top',
-#             'font': {
-#                 #'family': 'Arial, sans-serif',
-#                 'size':24,
-#                 'color': 'black'
-#             }
-#         },     
-#         xaxis_title='Month',     
-#         yaxis_title='Sales',
-#         paper_bgcolor='rgba(255,255,255,1)',
-#         plot_bgcolor='rgba(237,249,253,1)',
-#         )
-    
-#     cluster_counts = filtered_df['Cluster'].value_counts().reset_index()
-#     cluster_counts.columns = ['Cluster', 'count']
-#     custom_colors = ['#AAE5F4','#4991BA','#FFC357','#FFF7C5']
-#     pie_fig=px.pie(cluster_counts, 
-#                    values='count', 
-#                    names='Cluster',
-#                    color_discrete_sequence=custom_colors                  
-#                    )
-    
-#     pie_fig.update_layout(
-#         title={
-#             'text': f'Cluster Segmentation <br> for {selected_country}',
-#             'y':0.9,
-#             'x':0.43,
-#             'xanchor': 'center',
-#             'yanchor': 'top',
-#             'font': {
-#                 #'family': 'Arial, sans-serif',
-#                 'size':20,
-#                 'color': 'black'
-#             }
-#         }
-#     )
-#     return [trend_fig, pie_fig]
 
 @callback(
     Output('global-map', 'figure'),
     [Input('cluster-slider', 'value')]
 )
 def update_map(selected_segment):
-    cluster_name = cluster_mapping[selected_segment]
+    if selected_segment == 0:
+        filtered_df = df.copy()
+    else:
+        cluster_name = cluster_mapping[selected_segment]
     # Filter your dataset based on the selected segment
     # This assumes you have a way to filter your `df` based on the segmentation, which isn't shown here
-    filtered_df = df[df['Cluster'] == cluster_name]  # Example filter, adjust to your dataframe
+        filtered_df = df[df['Cluster'] == cluster_name]  # Example filter, adjust to your dataframe
     
     # Group by country and sum total sales again for the filtered dataset
     # country_sales = filtered_df.groupby('Country')['CustomerID'].unique().reset_index()
@@ -356,6 +321,7 @@ def update_map(selected_segment):
                                    color='Number_of_Customers',
                                    hover_name='Country',  # Make sure this is a column in country_sales
                                    projection='natural earth',
+                                   range_color=[0, 100]
                                    )
     global_map_fig.update_geos(
     lataxis_showgrid=False, lonaxis_showgrid=False,
@@ -384,28 +350,5 @@ def update_map(selected_segment):
         paper_bgcolor='rgb(34, 67, 74)',
         #plot_bgcolor='rgb(34, 67, 74)',
     )
-    
-    # # Generate a new choropleth figure based on the filtered dataset
-    # bubble_fig = px.choropleth(country_customers, locations='iso_alpha',
-    #                     color='Number_of_Customers',
-    #                     hover_name='Country', 
-    #                     projection='natural earth',
-    #                     )
-    
-    # bubble_fig.update_layout(
-    #     title={
-    #         'text': 'Customer Segmentation Heatmap',
-    #         'y':0.95,
-    #         'x':0.43,
-    #         'xanchor': 'center',
-    #         'yanchor': 'top',
-    #         'font': {
-    #             #'family': 'Arial, sans-serif',
-    #             'size':24,
-    #             'color': 'black'
-    #         }
-    #     },
-    #     plot_bgcolor='lightblue')  # Changes the plot's background color
-        
     
     return global_map_fig
